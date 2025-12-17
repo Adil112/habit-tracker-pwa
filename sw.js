c9c6f652d4520790ec1d4a4d5c40897fc9980128
adil <adil.200191@gmail.com>
2025-12-17 20:54:54 +0500

    deploy

--- sw.js
+++ sw.js
@@ -43,18 +43,18 @@ self.addEventListener('install', () => {
 
 self.addEventListener('fetch', () => {});
 
-// Clean up old caches
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-});
+// // Clean up old caches
+// self.addEventListener('activate', event => {
+//   const cacheWhitelist = [CACHE_NAME];
+//   event.waitUntil(
+//     caches.keys().then(cacheNames => {
+//       return Promise.all(
+//         cacheNames.map(cacheName => {
+//           if (cacheWhitelist.indexOf(cacheName) === -1) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+// });
